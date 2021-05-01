5f67b711dd60ce6e7228f87e092b7369aacfa38f
temilolasayo <alajikitemilola@gmail>
2021-04-30 21:05:40 -0700

    Fixed the overflow error for smaller devices

--- lib/main.dart
+++ lib/main.dart
@@ -11,46 +11,43 @@ class PortfolioApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.black45,
-          title: Text("@temilolasayo"),
+          title: Text("@temilolasayor"),
           centerTitle: true,
         ),
-        body: Container(
-          color: Colors.black26,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.0),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 8.0,
-                  ),
-                ],
-              ),
-              child: Padding(
-                  padding: const EdgeInsets.only(
-                    top: 15.0,
-                    bottom: 15.0,
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 50.0,
-                        ),
-                        child: Column(
+        body: SafeArea(
+          child: Container(
+            color: Colors.black26,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 8.0,
+                    ),
+                  ],
+                ),
+                child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 15.0,
+                      bottom: 15.0,
+                    ),
+                    child: Column(
+                      children: [
+                        Column(
                           children: [
                             //The cirle layout for the displayed image
                             Container(
                               width: 170.0,
                               height: 170.0,
                               decoration: BoxDecoration(
-                                // border: Border.all(
-                                //   color: Colors.black,
-                                //   width: 2.0,
-                                // ),
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 2.0,
+                                ),
                                 shape: BoxShape.circle,
                                 color: Colors.white,
                                 boxShadow: [
@@ -76,120 +73,120 @@ class PortfolioApp extends StatelessWidget {
                             ),
                           ],
                         ),
-                      ),
-                      //Displaying details about me, name, track, email, phone number etc
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 10.0,
+                        //Displaying details about me, name, track, email, phone number etc
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 10.0,
+                          ),
+                          child: Text(
+                            'Oluwasayo Temilola ALAJIKI',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
                         ),
-                        child: Text(
-                          'Oluwasayo Temilola ALAJIKI',
+                        Text(
+                          'Mobile Track',
                           style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                            fontStyle: FontStyle.italic,
+                            height: 1.5,
                           ),
                         ),
-                      ),
-                      Text(
-                        'Mobile Track',
-                        style: TextStyle(
-                          fontStyle: FontStyle.italic,
-                          height: 1.5,
-                        ),
-                      ),
-                      Text(
-                        'FLUTTER DEVELOPER INTERN',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.black26,
-                          height: 1.0,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 20.0,
-                        ),
-                        child: Container(
-                          padding: EdgeInsets.all(20),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Icon(
-                                      Icons.email,
-                                      color: Colors.red,
-                                    ),
-                                    Text(
-                                      'lola@gmail.com',
-                                      style: TextStyle(
-                                        fontSize: 12.0,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Icon(
-                                      Icons.call_rounded,
-                                      color: Colors.blue,
-                                    ),
-                                    Text(
-                                      '08027959837',
-                                      style: TextStyle(
-                                        fontSize: 12.0,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Icon(
-                                      Icons.place,
-                                      color: Colors.green,
-                                    ),
-                                    Text(
-                                      'Ilorin, NG',
-                                      style: TextStyle(
-                                        fontSize: 12.0,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
+                        Text(
+                          'FLUTTER DEVELOPER INTERN',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.black26,
+                            height: 1.0,
                           ),
                         ),
-                      ),
-                      Expanded(
-                        child: Icon(
-                          Icons.horizontal_rule,
-                          size: 50,
-                          color: Colors.black38,
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 20.0,
+                          ),
+                          child: Container(
+                            padding: EdgeInsets.all(20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Icon(
+                                        Icons.email,
+                                        color: Colors.red,
+                                      ),
+                                      Text(
+                                        'lola@gmail.com',
+                                        style: TextStyle(
+                                          fontSize: 12.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Icon(
+                                        Icons.call_rounded,
+                                        color: Colors.blue,
+                                      ),
+                                      Text(
+                                        '08027959837',
+                                        style: TextStyle(
+                                          fontSize: 12.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Icon(
+                                        Icons.place,
+                                        color: Colors.green,
+                                      ),
+                                      Text(
+                                        'Ilorin, NG',
+                                        style: TextStyle(
+                                          fontSize: 12.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                      ),
-                      Text(
-                        'An IT professional with over 5 years proven work experience in Web Design and Management. I am also passionate about helping businesses to solve problems.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 15,
-                          wordSpacing: 2.0,
+                        Expanded(
+                          child: Icon(
+                            Icons.horizontal_rule,
+                            size: 50,
+                            color: Colors.black38,
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        child: Icon(
-                          Icons.horizontal_rule,
-                          size: 50,
-                          color: Colors.black38,
+                        Text(
+                          'An IT professional with over 5 years proven work experience in Web Design and Management. I am also passionate about helping businesses to solve problems.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 15,
+                            wordSpacing: 2.0,
+                          ),
                         ),
-                      ),
-                    ],
-                  )),
+                        Expanded(
+                          child: Icon(
+                            Icons.horizontal_rule,
+                            size: 50,
+                            color: Colors.black38,
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
             ),
           ),
         ),
